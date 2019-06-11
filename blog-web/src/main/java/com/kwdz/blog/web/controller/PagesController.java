198e008fcb18a8a220b77d3923375c166f3ed0e4
vivtoum <vedyou@qq.com>
2019-06-11 18:09:43 +0800

    添加：验证规则 todo：后台取值

--- blog-web/src/main/java/com/kwdz/blog/web/controller/PagesController.java
+++ blog-web/src/main/java/com/kwdz/blog/web/controller/PagesController.java
@@ -43,6 +43,13 @@ public class PagesController {
         return modelAndView;
     }
 
+    /**
+     * 首页
+     *
+     * @param model
+     * @param request
+     * @return
+     */
     @GetMapping("index")
     public String index(Model model, HttpServletRequest request) {
         model.addAttribute("currentUser", request.getSession().getAttribute("user"));
@@ -62,6 +69,7 @@ public class PagesController {
     @GetMapping("resignation")
     public String resignation(Model model, HttpServletRequest request) throws FileNotFoundException {
         RemoteUserVo userVo = (RemoteUserVo) request.getSession().getAttribute("user");
+        //  todo 判断员工属于实习/试用/还是正式员工，不同性质last working day不同
         String joinTime = HolidayUtil.getLastWorkDate(30);
         model.addAttribute("currentUser", userVo);
         model.addAttribute("last_day", joinTime);
@@ -73,19 +81,21 @@ public class PagesController {
 
 
     /**
-     * 辞职申请页面
+     * 辞职面谈页面
      *
      * @param model
      * @param request
      * @return
      */
-    @GetMapping("resignation_meeting")
-    public String resignationMeeting(Model model, HttpServletRequest request) throws FileNotFoundException {
+    @GetMapping("resignation_meeting/{step}")
+    public String resignationMeeting(Model model, HttpServletRequest request, @PathVariable("step") String step) throws FileNotFoundException {
         RemoteUserVo userVo = (RemoteUserVo) request.getSession().getAttribute("user");
+        //  todo 判断员工属于实习/试用/还是正式员工，不同性质last working day不同
         String joinTime = HolidayUtil.getLastWorkDate(30);
         model.addAttribute("currentUser", userVo);
         model.addAttribute("_form", toTree(request));
         model.addAttribute("last_day", joinTime);
+        model.addAttribute("_step", step);
         // "test"是test.html的名，
         // "iframe_div"是test.html中需要刷新的部分标志,
         // 在标签里加入：th:fragment="iframe_div"
